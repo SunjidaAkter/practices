2ba8bbe85ce10a40806d77dbc289221afa7595ed
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-04-07 23:41:43 +0600

    input-output problem solved

--- practice-2.5/input-output.c
+++ practice-2.5/input-output.c
@@ -1,7 +1,25 @@
+/* 
+
+You need to print the lines given below in the same format as it is -
+
+ “Recently I heard that you’ve achieved 95% marks in your exam. 
+ This is brilliant! 
+ I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.”
+
+
+
+ Sample Input
+ There is no input
+ Sample Output
+ Recently I heard that you’ve achieved 95% marks in your exam. 
+ This is brilliant! 
+ I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.
+
+*/
+
 #include<stdio.h>
+
 int main(){
-    for(int i=0;i>=0;i++){
-        printf("hello\n");
-    }
+    printf("Recently I heard that you’ve achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you’ll shine in your life!\tGood luck with all the barriers(/\\) in your life.");
     return 0;
 }
