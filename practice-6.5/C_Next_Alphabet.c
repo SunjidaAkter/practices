07fddb585b78e6315464c4e4de6f12f9c695eca0
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-04-13 13:59:45 +0600

    roughly completed 5 problems

---
+++ practice-6.5/C_Next_Alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char c;
+    scanf("%c",&c);
+    if(c>=97 || c<122){
+        printf("%c",c+1);
+    }else if(c==122){
+        printf("%c",c-1);
+    }
+    return 0;
+}
