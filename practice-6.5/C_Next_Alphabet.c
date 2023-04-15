437181718e904d5317e6fab4f5001ac54da6d92e
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-04-15 13:44:32 +0600

    set up done

--- practice-6.5/C_Next_Alphabet.c
+++ practice-6.5/C_Next_Alphabet.c
@@ -2,11 +2,10 @@
 int main(){
     char c;
     scanf("%c",&c);
-    if(c==122){
-        printf("%c",c-25);
-    }else if(c<=121 || c>=97){
+    if(c<=121 && c>=97){
         printf("%c",c+1);
+    }else if(c==122){
+        printf("%c",c-25);
     }
-    // printf("%c",c-25);
     return 0;
 }
