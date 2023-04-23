b3308aa91305599a21701d664876b75c46c221ff
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-04-23 10:02:36 +0600

    palindrome is solved by another way

--- practice-10.5/I_Palindrome.c
+++ practice-10.5/I_Palindrome.c
@@ -2,7 +2,8 @@
 #include<string.h>
 int main(){
     char a[1001];
-    fgets(a,1001,stdin);
+    // fgets(a,1001,stdin);//eta just string line input neaer somoi use korte hobe
+    scanf("%s",&a);
     int is_palindrome=1;
     int i=0,j=strlen(a)-1;
     while(i<j){
@@ -21,3 +22,37 @@ int main(){
     }
     return 0;
 }
+//another way
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//     char a[1001],b[1001];
+//     scanf("%s",&a);
+//     for(int i=0;a[i]!='\0';i++){
+//         b[i]=a[i];
+//     }
+//     int i=0,j=strlen(a)-1;
+//     while(i<j){
+//         char temp=b[i];
+//         b[i]=b[j];
+//         b[j]=temp;
+//         i++;
+//         j--;
+//     }
+//     int flag;
+//     for(int i=0;i<strlen(a);i++){
+//         if(a[i]!=b[i]){
+//             flag=0;
+//             break;
+//         }else{
+//             flag=1;
+//             continue;
+//         }
+//     }
+//     if(flag){    
+//         printf("YES");
+//     }else{
+//         printf("NO");
+//     }
+//     return 0;
+// }
