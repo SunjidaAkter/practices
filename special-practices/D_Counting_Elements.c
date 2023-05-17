0c12fbbf253553448dec9bca8af74e43bd5bb2ec
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-05-17 13:28:00 +0600

    six problem solved

--- special-practices/D_Counting_Elements.c
+++ special-practices/D_Counting_Elements.c
@@ -4,7 +4,7 @@
 #include<stdlib.h>
 #include<limits.h>
 int main(){
-    int n;
+    int n,count1=0,count=0;
     scanf("%d",&n);
     int a[n];
     for(int i=0;i<n;i++){
@@ -12,10 +12,20 @@ int main(){
     }
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
-            if(a[i]==(a[j]-1)){
-                printf("%d ",a[i]);
+            if(a[i]>a[j]){
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
             }
         }
     }
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if((a[i]+1==(a[j]))&&(a[j]!=a[j+1])){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
     return 0;
 }
