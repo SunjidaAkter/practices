3b3c01c2596a05e98ad3fd8c825263a237a233e8
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-05-19 00:35:49 +0600

    all problems solved

--- special-practices/G_Even_Hate_Odd.c
+++ special-practices/G_Even_Hate_Odd.c
@@ -4,15 +4,16 @@
 #include<stdlib.h>
 #include<limits.h>
 int main(){
-    int test,even=0,odd=0;
+    int test,max;
     scanf("%d",&test);
     for(int i=0;i<test;i++){
+        int even=0,odd=0;
         int n;
         scanf("%d",&n);
         int a[n];
         for(int j=0;j<n;j++){
-            scanf("%d",&a[i]);
-            if(a[i]%2==0){
+            scanf("%d",&a[j]);
+            if(a[j]%2==0){
                 even++;
             }else{
                 odd++;
@@ -23,7 +24,7 @@ int main(){
         }else if(odd==even){
             printf("%d\n",0);
         }else{
-            printf("%d\n",(abs(even-odd))/2);
+            printf("%d\n",abs(even-odd)/2);
         }
     }
     return 0;
--- special-practices/O_Sort_String.c
+++ special-practices/O_Sort_String.c
@@ -4,22 +4,21 @@
 #include<stdlib.h>
 #include<limits.h>
 int main(){
-    int n,k=0;
-    char str[n+1],s[n+1];
-    while((scanf("%c",&s[k]))!=EOF){
-        str[k]=s[k];
-        k++;
-    }
-    for(int i=0;i<(strlen(str)-1);i++){
-        for(int j=i+1;str[j]!='\0';j++){
-            // int num=strcmp(str[i],str[j]);
-            if(str[i]>str[j]){
-                char temp=str[i];
-                str[i]=str[j];
-                str[j]=temp;
-            }
+    int n,cs[26]={0};
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        char c;
+        scanf("%c",&c);
+        if(c>96){
+            cs[c-'a']++;
         }
     }
-    printf("%s",str);
+    for(int i=0;i<26;i++){
+        while(cs[i]!=0){
+            printf("%c",i+'a');
+            cs[i]--;
+        }
+    }
+
     return 0;
 }
