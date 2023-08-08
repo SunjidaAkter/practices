e31f0223361e63ed0dd5afbe25099c3abb6592c2
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-08 17:17:12 +0600

    first problem solved

--- algo/practice-3.5/graph_reverse.cpp
+++ algo/practice-3.5/graph_reverse.cpp
@@ -15,7 +15,27 @@
  */
 #include<bits/stdc++.h>
 using namespace std;
+
+const int N=1e5+5;
+vector<int>adj[N];
+bool visited[N];
+void dfs(int u){
+    visited[u]=true;
+    for(int v:adj[u]){
+        if(visited[v])continue;
+        dfs(v);
+    cout<<v<<" ";
+    }
+}
 int main(){
-    
+    int m,n;cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            int u,v;cin>>u>>v;
+            adj[u].push_back(v);
+        }
+    }
+    dfs(1);
+    cout<<1;
     return 0;
 }
