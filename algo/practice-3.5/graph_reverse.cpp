d46eb7002ec197babf6c105d55210e1d1527df2a
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-08 16:42:44 +0600

    completed

---
+++ algo/practice-3.5/graph_reverse.cpp
@@ -0,0 +1,21 @@
+/*
+ * Question: You will be given an undirected graph. Print its DFS traversal in reverse order. Consider root as 1.
+
+*Sample Input
+? 4 
+? 4
+? 1 2
+? 4 3
+? 3 2
+? 2 4
+*Sample Output
+? 3 4 2 1
+? ( 4 3 2 1 is also valid ans)
+
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    return 0;
+}
