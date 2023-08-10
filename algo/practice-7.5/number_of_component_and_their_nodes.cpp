d3d81515a80d883d89736a0cc5cf4617e695b954
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-10 16:06:40 +0600

    completed

---
+++ algo/practice-7.5/number_of_component_and_their_nodes.cpp
@@ -0,0 +1,30 @@
+/*
+*Question: You will be given an undirected graph as input. Now count its connected components and print it. Then print All the nodes in a connected component. See the sample output for more clarification.
+
+!Sample Input
+?6
+?4
+?1 2
+?2 3
+?3 4
+?5 6
+!Sample Output
+?2
+?Component 1 : 1 2 3 4
+?Component 2 : 5 6
+!Sample Input
+?8
+?4
+?1 2
+?2 3
+?3 4
+?5 6
+!Sample Output
+?4
+?Component 1 : 1 2 3 4
+?Component 2 : 5 6
+?Component 3 : 7
+?Component 4 : 8
+
+
+*/
