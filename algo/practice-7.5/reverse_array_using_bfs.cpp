f60771240acf882da4842c05c873193e9bec0ad7
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-10 15:49:22 +0600

    completed

---
+++ algo/practice-7.5/reverse_array_using_bfs.cpp
@@ -0,0 +1,15 @@
+/*
+*Question: You will be given an undirected graph. Print its BFS traversal in reverse order. Consider root as 1.
+
+!Sample Input
+?4 
+?4
+?1 2
+?3 4
+?1 3
+?2 4
+!Sample Output
+?4 3 2 1
+?(4 2 3 1 is also valid ans)
+
+*/
