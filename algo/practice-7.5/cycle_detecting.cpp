6d47b78a9d39c305a3069630de4b28d6465f3495
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-10 15:56:19 +0600

    completed

--- algo/practice-7.5/cycle_detecting.cpp
+++ algo/practice-7.5/cycle_detecting.cpp
@@ -1,23 +1,25 @@
 /*
-Question: You will be given an undirected graph as input. Now print “YES” if there is a cycle and print “NO” if there isn’t any cycle. Consider root as 1.
+*Question: You will be given an undirected graph as input. Now print “YES” if there is a cycle and print “NO” if there isn’t any cycle. Consider root as 1.
 
 
-Sample Input
-Sample Output
-4
-4
-1 2
-2 3
-3 4
-4 2
-YES
-6
-4
-1 2
-2 3
-3 4
-5 6
-NO
+!Sample Input
+?4
+?4
+?1 2
+?2 3
+?3 4
+?4 2
+!Sample Output
+?YES
+!Sample Input
+?6
+?4
+?1 2
+?2 3
+?3 4
+?5 6
+!Sample Output
+?NO
 
 
 */
