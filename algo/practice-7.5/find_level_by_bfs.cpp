cc17f9ebff3880a46870780ace417b34164af2f0
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-10 15:52:17 +0600

    completed

---
+++ algo/practice-7.5/find_level_by_bfs.cpp
@@ -0,0 +1,30 @@
+/*
+*Question: You will be given an undirected graph as input. This graph will contain only one connected component. The edge number will be exactly node-1. Then take a node from the input and print its level. Consider root as 1.
+
+!Sample Input
+7 
+6
+1 2
+2 4
+2 5
+1 3
+3 6
+1 7
+7
+!Sample Output
+Level of 7 = 1
+!Sample Input
+8 
+7
+1 2
+2 4
+2 5
+1 3
+3 6
+1 7
+4 8
+8
+!Sample Output
+Level of 8 = 3
+
+*/
