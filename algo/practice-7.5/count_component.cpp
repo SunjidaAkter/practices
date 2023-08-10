d064e0dceaa7b830009447daa7180c97ca5a5f5d
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-10 16:02:01 +0600

    completed

---
+++ algo/practice-7.5/count_component.cpp
@@ -0,0 +1,25 @@
+/*
+*Question: You will be given an undirected graph as input. Now count its connected components and print it.
+
+!Sample Input
+?6
+?4
+?1 2
+?2 3
+?3 4
+?5 6
+!Sample Output
+?2
+!Sample Input
+?7
+?4
+?1 2
+?2 3
+?3 4
+?5 6
+!Sample Output
+?3
+
+
+*/
+
