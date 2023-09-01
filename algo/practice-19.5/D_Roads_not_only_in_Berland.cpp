b83d7b64c968aa8008e4bc2e31a6246ceb82dc4c
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-01 23:08:36 +0600

    mission accomplished 🔥🚀🔥

---
+++ algo/practice-19.5/D_Roads_not_only_in_Berland.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1e5+5;
+int parent[N];
+int parentSize[N];
+void dsu_set(int n){
+    for(int i=1;i<=n;i++){
+        parent[i]=-1;
+        parentSize[i]=1;
+    }
+}
+int dsu_find(int n){
+    while(parent[n]!=-1){
+        n=parent[n];
+    }
+    return n;
+}
+void dsu_union(int a,int b){
+    int leaderA=dsu_find(a);
+    int leaderB=dsu_find(b);
+    if(leaderA!=leaderB){
+        if(parentSize[leaderA]>parentSize[leaderB]){
+            parent[leaderB]=leaderA;
+            parentSize[leaderA]+=parentSize[leaderB];
+        }else{
+            parent[leaderA]=leaderB;
+            parentSize[leaderB]+=parentSize[leaderA];
+        }
+    }
+}
+int main(){
+    int n;cin>>n;int e=n-1;
+    vector<pair<int,int>>v;
+    vector<pair<int,int>>v1;
+    vector<pair<int,int>>v2;
+    dsu_set(n);
+    while(e--){
+        int x,y;cin>>x>>y;
+        v.push_back({x,y});
+    }
+    int cnt=0;
+    for(pair<int,int> val:v){
+        int a=val.first;
+        int b=val.second;
+        int leaderA=dsu_find(a);
+        int leaderB=dsu_find(b);
+        if(leaderA==leaderB){
+            v1.push_back(val);
+        }else{
+            v2.push_back(val);
+            dsu_union;
+        }
+    }
+    cout<<v1.size()<<endl;
+    for(auto val:v1)cout<<val.first<<" "<<val.second<<" ";
+    for(auto val:v2)cout<<val.first<<" "<<val.second<<" ";
+    return 0;
+}
