89862e23a64f01d487a694464645e941238549ce
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-09 16:35:29 +0600

    mission accomplished 🔥🚀🔥

--- algo/practice-22.5/X_The_maximum_path_sum_by_memoization.cpp
+++ algo/practice-22.5/X_The_maximum_path_sum_by_memoization.cpp
@@ -3,13 +3,13 @@ using namespace std;
 typedef pair<int,int>pii;
 #define ll long long
 int row,column;
-int arr[1005][1005];
-int dp[1005][1005];
-int knapsack(int n,int m){
+ll arr[15][15];
+ll dp[1005][1005];
+ll knapsack(int n,int m){
     if(row<=n||column<=m)return 0;
     if(dp[n][m]!=-1)return dp[n][m];
-    int choice1=knapsack(n+1,m)+arr[n][m];
-    int choice2=knapsack(n,m+1)+arr[n][m];
+    ll choice1=knapsack(n+1,m)+arr[n][m];
+    ll choice2=knapsack(n,m+1)+arr[n][m];
     return dp[n][m]=max(choice1,choice2);
 }
 int main(){
@@ -19,8 +19,8 @@ int main(){
             cin>>arr[i][j];
         }
     }
-    for(int i=0;i<row;i++){
-        for(int j=0;j<column;j++){
+    for(int i=0;i<=row;i++){
+        for(int j=0;j<=column;j++){
             dp[i][j]=-1;
         }
     }
