40f184e6d98fd7de5caac1dc8342763e6aaedca5
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-09-12 19:46:28 +0600

    mission accomplished 🔥🚀🔥

--- algo/practice-22.5/X_The_maximum_path_sum_by_memoization.cpp
+++ algo/practice-22.5/X_The_maximum_path_sum_by_memoization.cpp
@@ -27,3 +27,35 @@ int main(){
     cout<<knapsack(0,0);
     return 0;
 }
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define lli long long int
+// int n, m;
+// int sum = INT_MIN;
+// int a[10][10];
+// void max_sum(int i, int j, int s)
+// {
+//     if (i >= n || j >= m)
+//         return;
+//     if (i == n - 1 && j == m - 1)
+//         sum = max(sum, s+a[i][j]);
+
+//     max_sum(i + 1, j, s + a[i][j]);
+//     max_sum(i, j + 1, s + a[i][j]);
+// }
+
+// int main()
+// {
+//     cin >> n >> m;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             cin >> a[i][j];
+//         }
+//     }
+
+//     max_sum(0, 0, 0);
+//     cout << sum << endl;
+//     return 0;
+// }
