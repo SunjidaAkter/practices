36bb379820485bbe4eca489221eea503495d69b6
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-10 16:42:25 +0600

    mission accomplished 🔥🚀🔥

---
+++ pratice_contest_1/A_The_Good_Array.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,k;cin>>n>>k;
+    ll ans=ceil(n*1.0/k);
+    if(n%k>1||((n%k)==0&&k>1))ans++;
+    cout<<ans<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
